8c7aa5f92630526a86beb55f866c1b68eff6561b
yandizg <yandi@44.com>
2019-09-11 08:29:29 +0800

    day10rk

---
+++ src/main/java/com/yd/Service/DateUtil.java
@@ -0,0 +1,19 @@
+package com.yd.Service;
+
+import java.util.Scanner;
+
+public class DateUtil {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入出生的年份");
+		Integer sum = 0;
+		Integer num = sc.nextInt();
+		sum = 2019-num;
+		System.out.println("您今年"+sum+"岁了呢");
+		
+		
+	}
+	
+}
